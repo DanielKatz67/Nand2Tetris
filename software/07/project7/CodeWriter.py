69d6696dc171881814762dd8eb61e8d9a9cec711
Daniel Katz <daniel.katz@post.runi.ac.il>
2024-01-28 16:28:04 +0200

    project 7 - need to fix PointerTest

--- software/07/project7/CodeWriter.py
+++ software/07/project7/CodeWriter.py
@@ -5,6 +5,7 @@ class CodeWriter:
          Opens the output file/stream, and gets ready ro parse it
         """
         self.file = open(output_file_path, 'a')
+        self.jumpCounter=0
 
     # noinspection PyTypeChecker
     def writeArithmetic(self, command):
@@ -15,17 +16,21 @@ class CodeWriter:
         """
         if command in ['add', 'sub', 'and', 'or']:
             self._stack_manipulation()
-            if command == 'add': self.file.write('M=M+D\n')
-            if command == 'sub': self.file.write('M=M-D\n')
-            if command == 'and': self.file.write('M=M&D\n')
-            if command == 'or' : self.file.write('M=M|D\n')
-
-        if command == 'neg': self.file.write('D=0\n@SP\nA=M-1\nM=D-M\n')
-        if command == 'not': self.file.write('@SP\nA=M-1\nM=!M\n')
-
-        if command == 'eq': self.file.write(self._translateComparison('JNE','_EQ'))   # JNE is not 0
-        if command == 'gt': self.file.write(self._translateComparison('JLE', '_GT'))  # JLE is <=0
-        if command == 'lt': self.file.write(self._translateComparison('JGE', '_LT'))  # JGE is >=0
+            if command == 'add': self.file.write('   M=M+D\n')
+            if command == 'sub': self.file.write('   M=M-D\n')
+            if command == 'and': self.file.write('   M=M&D\n')
+            if command == 'or' : self.file.write('   M=M|D\n')
+        if command == 'neg': self.file.write('   D=0\n   @SP\n   A=M-1\n   M=D-M\n')
+        if command == 'not': self.file.write('   @SP\n   A=M-1\n   M=!M\n')
+        if command == 'eq':
+            self._translateComparison('JNE','EQ_')   # JNE is not 0
+            self.jumpCounter += 1
+        if command == 'gt':
+            self._translateComparison('JLE', 'GT_')  # JLE is <=0
+            self.jumpCounter += 1
+        if command == 'lt':
+            self._translateComparison('JGE', 'LT_')  # JGE is >=0
+            self.jumpCounter += 1
 
     def _stack_manipulation(self):
         """
@@ -33,30 +38,30 @@ class CodeWriter:
         then do the operation and write it over the second place
         """
         self.file.write("//stack manipulation\n")
-        self.file.write("@SP\nAM=M-1\nD=M\nA=A-1\n")
+        self.file.write("   @SP\n   AM=M-1\n   D=M\n   A=A-1\n")
 
     def _translateComparison(self, jump_kind: str, comparison_kind: str):
         """
         Handling comparison 'eq', 'gt', 'lt'
         """
         self._stack_manipulation()
-        self.file.write(f"""\
-        D=M-D
-        //Comparison and conditional jump
-        @{comparison_kind}CONDITION_IS_FALSE
-        D;{jump_kind}
-        //Setting the result for true condition. -1 is true
-        @SP
-        A=M-1
-        M=-1
-        @{comparison_kind}CONDITION_IS_TRUE
-        0;JMP
-        //Handling false condition. -1 is false
-        ({comparison_kind}CONDITION_IS_FALSE)
-        @SP
-        A=M-1
-        M=0
-        ({comparison_kind}CONDITION_IS_TRUE) """)
+        self.file.write(
+        f"   D=M-D\n"
+        f"//Comparison and conditional jump\n"
+        f"   @{comparison_kind}{self.jumpCounter}_CONDITION_IS_FALSE\n"
+        f"   D;{jump_kind}\n"
+        f"//Setting the result for true condition. -1 is true\n"
+        f"   @SP\n"
+        f"   A=M-1\n"
+        f"   M=-1\n"
+        f"   @{comparison_kind}{self.jumpCounter}_CONDITION_IS_TRUE\n"
+        f"   0;JMP\n"
+        f"//Handling false condition. 0 is false\n"
+        f"({comparison_kind}{self.jumpCounter}_CONDITION_IS_FALSE)\n"
+        f"   @SP\n"
+        f"   A=M-1\n"
+        f"   M=0\n"
+        f"({comparison_kind}{self.jumpCounter}_CONDITION_IS_TRUE)\n")
 
     def writePushPop(self, c_type, segment, i):
         """
@@ -67,8 +72,8 @@ class CodeWriter:
             if segment == 'pointer' and i == 0 : self._translatePush("THIS", i)
             if segment == 'pointer' and i == 1 : self._translatePush("THAT", i)
             if segment == 'constant'           : self._translatePush('constant', i)
-            if segment == 'static'             : self._translatePush(16, i)
-            if segment == 'temp'               : self._translatePush(5, i)
+            if segment == 'static'             : self._translatePush(str(16+int(i)), i)
+            if segment == 'temp'               : self._translatePush("R5", str(int(i)+5))
             if segment == 'local'              : self._translatePush('LCL', i)
             if segment == 'argument'           : self._translatePush('ARG', i)
             if segment == 'this'               : self._translatePush('THIS', i)
@@ -76,70 +81,85 @@ class CodeWriter:
         else:
             if segment == 'pointer' and i == 0 : self._translatePop("THIS", i)
             if segment == 'pointer' and i == 1 : self._translatePop("THAT", i)
-            if segment == 'static'             : self._translatePop(16, i)
-            if segment == 'temp'               : self._translatePop(5, i)
+            if segment == 'static'             : self._translatePop(str(16+int(i)), i)
+            if segment == 'temp'               : self._translatePop("R5", str(int(i)+5))
             if segment == 'local'              : self._translatePop('LCL', i)
             if segment == 'argument'           : self._translatePop('ARG', i)
             if segment == 'this'               : self._translatePop('THIS', i)
             if segment == 'that'               : self._translatePop('THAT', i)
 
     def _translatePush(self, segment, i):
-        addition_not_for_pointer = ''
-        if segment != 'pointer':
-            addition_not_for_pointer = f"""
-            @{i}
-            A=D+A
-            D=M\n
-            """
+        addition_not_for_direct = ''
+        if segment not in ['pointer','static']:
+            addition_not_for_direct = (
+            f"   @{i}\n"
+            f"   A=D+A\n"
+            f"   D=M\n")
 
-        suffix = f"""\
-        @SP
-        A=M
-        M=D
-        //SP++
-        @SP
-        M=M+1
-        """
+
+        suffix = (
+        "   @SP\n"
+        "   A=M\n"
+        "   M=D\n"
+        "//SP++\n"
+        "   @SP\n"
+        "   M=M+1\n")
 
         if segment == 'constant':
-            self.file.write(f"""\
-            @index
-            D=A
-            {suffix}
-            """)
+            self.file.write(
+            f"   @{i}\n"
+            f"   D=A\n"
+            f"{suffix}")
+
         else:
-            self.file.write(f"""\")
-            //D=RAM[i+{segment}Pointer]
-            @{segment}
-            D=M{addition_not_for_pointer}
-            //RAM[SP]=D
-            {suffix}
-            """)
+            self.file.write(
+            f"//D=RAM[i+{segment}Pointer]\n"
+            f"   @{segment}\n"
+            f"   D=M\n{addition_not_for_direct}\n"
+            f"//RAM[SP]=D\n"
+            f"{suffix}")
 
     def _translatePop(self, segment, i):
-        addition_not_for_pointer = 'D=A'
-        if segment != 'pointer':
-            addition_not_for_pointer = f"""
-            D=M
-            @index
-            D=D+A
-            """
+        addition_not_for_direct = '   D=A'
+        if segment not in ['pointer','static']:
+            addition_not_for_direct = (
+            f"   D=M\n"
+            f"   @{i}\n"
+            f"   D=D+A\n")
 
-        self.file.write(f"""\
-        @{segment}
-        {addition_not_for_pointer}
-        @R15
-        M=D
-        @SP
-        AM=M-1
-        D=M
-        @R15
-        A=M
-        M=D
-        """)
+
+        self.file.write(
+        f"   @{segment}\n"
+        f"{addition_not_for_direct}"
+        f"   @R15\n"
+        f"   M=D\n"
+        f"   @SP\n"
+        f"   AM=M-1\n"
+        f"   D=M\n"
+        f"   @R15\n"
+        f"   A=M\n"
+        f"   M=D\n")
+
+    def write_vm_line_as_comment(self, line):
+        self.file.write(f"//{line}\n")
+
+    def write_vm_file_name_comment(self, path):
+        self.file.write(f"\n//{path.split('/')[-1]}:\n")
+
+    def set_SP_to_256(self):
+        self.file.write(
+        "//SetSP = 256\n"
+        "   @256\n"
+        "   D=A\n"
+        "   @SP\n"
+        "   M=D\n")
 
     def close(self):
         """
         closes the output file
         """
+        self.file.write(
+        "(END)\n"
+        "   @END\n"
+        "   0;JMP\n")
         self.file.close()
--- software/07/project7/main.py
+++ software/07/project7/main.py
@@ -1,11 +1,60 @@
+import os
+import glob
+import sys
 import Parser
 import CodeWriter
 
-def main():
+def main(input_path):
+    PATH=input_path
 
+    if os.path.isfile(PATH) and PATH.endswith('.vm'):
+        output_file = PATH.split('.')[0]+'.asm'
+        writer = CodeWriter.CodeWriter(output_file)
+        writer.set_SP_to_256()
+        translate_single_file_vm_to_asm(PATH, writer)
+        writer.close()
 
+    if os.path.isdir(PATH) :
+        vm_files = glob.glob(os.path.join(PATH, '*.vm'))
+        # Ensure the path ends with '/' to correctly calculate the directory name
+        if not PATH.endswith('/'): PATH += '/'
+        # Determine the name of the output .asm file
+        dir_name = os.path.basename(os.path.normpath(PATH))
+        output_file = os.path.join(PATH, dir_name + '.asm')
+        writer = CodeWriter.CodeWriter(output_file)
+        writer.set_SP_to_256()
 
-# Press the green button in the gutter to run the script.
+        for vm_file in vm_files:
+            writer.write_vm_file_name_comment(vm_file)
+            translate_single_file_vm_to_asm(vm_file, writer)
+        writer.close()
+
+def translate_single_file_vm_to_asm(inputFile, writer):
+    parser = Parser.Parser(inputFile)
+    while parser.hasMoreLines() :
+        parser.advance()
+        writer.write_vm_line_as_comment(parser.current_line)
+        commandType = parser.commandType()
+        arg1 = ''
+        arg2 = ''
+        if commandType != 'C_RETURN' :
+            arg1 = parser.arg1()
+        if commandType in ['C_PUSH', 'C_POP', 'C_FUNCTION', 'C_CALL']:
+            arg2 = parser.arg2()
+
+        if commandType == 'C_ARITHMETIC':
+            writer.writeArithmetic(arg1)
+
+        if commandType in ['C_PUSH', 'C_POP']:
+            writer.writePushPop(commandType, arg1, arg2)
+    parser.close()
+
 if __name__ == '__main__':
-    main()
+    dir_path = '/Users/daniel/Desktop/Nand2Tetris/projects/software/07/MemoryAccess/PointerTest/PointerTest.vm'
+    # if len(sys.argv) != 2:
+    #     print("Usage: VMtranslator <directory path>")
+    #     sys.exit(1)
+    #
+    # dir_path = sys.argv[1]
+    main(dir_path)
 
